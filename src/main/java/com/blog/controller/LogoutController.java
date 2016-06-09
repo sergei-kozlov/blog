537f3ed96cd078d48f05fe5a7b0b74cf5c360bb2
sergei-kozlov <svkozlow@gmail.com>
2016-06-09 15:26:53 +0300

    09.06.16

---
+++ src/main/java/com/blog/controller/LogoutController.java
@@ -0,0 +1,15 @@
+package com.blog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class LogoutController {
+
+    @RequestMapping(value = "/logout", method = RequestMethod.GET)
+    public String secure() {
+        return "index";
+    }
+
+}
